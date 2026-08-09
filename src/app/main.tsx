c6e1f297051c830349604a4d4ea7e063a0ee9aa2
Ravi Kumar <ravikr08.in@gmail.com>
2026-08-09 19:11:57 +0530

    Added React toolkit for state replacing zustand

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import {Router} from './Router.tsx'
+import { Router } from './Router.tsx'
+import { Provider } from 'react-redux'
+import { appStore } from './appStore'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={Router} />
+    <Provider store={appStore}>
+      <RouterProvider router={Router} />
+    </Provider>
   </StrictMode>,
 )
